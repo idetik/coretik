56d537237f35128b5f04e4450866b323f9484e91
David <david.daugreilh@gmail.com>
2021-02-21 22:38:10 +0100

    bugfix - schemaviewer

--- src/Core/Services/Admin/SchemaViewer/views/wrapper.php
+++ src/Core/Services/Admin/SchemaViewer/views/wrapper.php
@@ -6,16 +6,20 @@
             <div class="schema-box">
                 <h3>Post types</h3>
                 <?php
-                foreach (Coretik\App::schema()->type('post')->all() as $builder) {
-                    include 'post-type-single.php';
+                if (Coretik\App::schema()->has('post')) {
+                    foreach (Coretik\App::schema()->type('post')->all() as $builder) {
+                        include 'post-type-single.php';
+                    }
                 }
                 ?>
             </div>
             <div class="schema-box">
                 <h3>Taxonomies</h3>
                 <?php
-                foreach (Coretik\App::schema()->type('taxonomy')->all() as $builder) {
-                    include 'taxonomy-single.php';
+                if (Coretik\App::schema()->has('taxonomy')) {
+                    foreach (Coretik\App::schema()->type('taxonomy')->all() as $builder) {
+                        include 'taxonomy-single.php';
+                    }
                 }
                 ?>
             </div>
@@ -24,8 +28,10 @@
             <div class="schema-box">
                 <h3>User types</h3>
                 <?php
-                foreach (Coretik\App::schema()->type('user')->all() as $builder) {
-                    include 'user-type-single.php';
+                if (Coretik\App::schema()->has('user')) {
+                    foreach (Coretik\App::schema()->type('user')->all() as $builder) {
+                        include 'user-type-single.php';
+                    }
                 }
                 ?>
             </div>
