e3cad3eb1c9a6a418aba10d493c4a5fa9fb23519
David <david.daugreilh@gmail.com>
2023-09-13 16:10:54 +0200

    feat: String util class provide a 'toString' method from mixed parameter

--- src/Core/Utils/Str.php
+++ src/Core/Utils/Str.php
@@ -2,6 +2,8 @@
 
 namespace Coretik\Core\Utils;
 
+use Closure;
+
 class Str
 {
     public static function camel(string $src): string
@@ -12,5 +14,16 @@ class Str
     public static function humanize(string $entry, string $separator = '_'): string
     {
         return \trim(\strtolower((string) \preg_replace(['/([A-Z])/', \sprintf('/[%s\s]+/', $separator)], ['_$1', ' '], $entry)));
+    }
+
+    public static function toString(mixed $value): string
+    {
+        return match (true) {
+            is_string($value) => $value,
+            is_bool($value) => $value ? 'true' : 'false',
+            is_array($value) => implode(', ', array_map(__METHOD__, $value)),
+            $value instanceof Closure => Dump::closure($value),
+            default => strval($value)
+        };
     }
 }
