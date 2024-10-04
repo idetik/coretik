c5d9d3b075dc214729b31ae3a18dc762a7ca55d1
David Daugreilh <david.daugreilh@idetik.fr>
2024-10-04 09:43:22 +0200

    fix: Populate builder interface

--- src/Core/Builders/Interfaces/ModelableInterface.php
+++ src/Core/Builders/Interfaces/ModelableInterface.php
@@ -8,7 +8,7 @@ use Coretik\Core\Query\Interfaces\QuerierInterface;
 interface ModelableInterface
 {
     public function factory(callable $factory);
-    public function model($initializer): Model;
+    public function model($id = null, $initializer = null, bool $refresh = false): Model;
     public function querier(callable $querier);
     public function query(): QuerierInterface;
     public function wpObject(int $id);
