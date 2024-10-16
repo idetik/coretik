ad3a735c8c63a450fb25284efe055d20696a21a5
David Daugreilh <david.daugreilh@idetik.fr>
2024-10-16 10:13:29 +0200

    feat: Add boot<Trait>Once method to manage global WP hooks

--- src/Core/Models/Traits/Bootable.php
+++ src/Core/Models/Traits/Bootable.php
@@ -2,9 +2,13 @@
 
 namespace Coretik\Core\Models\Traits;
 
+use Coretik\Core\Utils\Classes;
+
 trait Bootable
 {
     protected static $booted = [];
+    protected static $traitInitializers = [];
+    protected static $traitBooted = [];
 
     protected static function bootIfNotBooted()
     {
@@ -16,13 +20,40 @@ trait Bootable
 
     protected static function boot()
     {
+        static::bootTraits();
+    }
+
+    protected static function bootTraits()
+    {
         // Boot traits
-        foreach (class_uses(get_called_class()) as $traitNamespace) {
-            $ref = new \ReflectionClass($traitNamespace);
-            $traitName = $ref->getShortName();
-            $boot = 'boot' . $traitName;
-            if (method_exists(static::class, $boot)) {
-                forward_static_call([static::class, $boot]);
+        $class = static::class;
+        $booted = [];
+        $traits = Classes::classUsesDeep($class);
+
+        static::$traitInitializers[$class] = [];
+
+        foreach ($traits as $trait) {
+
+            // Boot once
+            $method = 'boot'.class_basename($trait).'Once';
+            if (method_exists($class, $method) && ! in_array($method, static::$traitBooted)) {
+                forward_static_call([$class, $method]);
+                static::$traitBooted[] = $method;
+            }
+
+            // Classic boot
+            $method = 'boot'.class_basename($trait);
+            if (method_exists($class, $method) && ! in_array($method, $booted)) {
+                forward_static_call([$class, $method]);
+                $booted[] = $method;
+            }
+
+            // Register initializers
+            if (method_exists($class, $method = 'initialize'.class_basename($trait))) {
+                static::$traitInitializers[$class][] = $method;
+                static::$traitInitializers[$class] = array_unique(
+                    static::$traitInitializers[$class]
+                );
             }
         }
     }
--- src/Core/Models/Traits/Initializable.php
+++ src/Core/Models/Traits/Initializable.php
@@ -9,13 +9,20 @@ trait Initializable
     protected function initialize()
     {
         // Initizalize traits
-        foreach (Classes::classUsesDeep($this) as $traitNamespace) {
-            $ref = new \ReflectionClass($traitNamespace);
-            $traitName = $ref->getShortName();
-            $initializer = 'initialize' . $traitName;
-            if (method_exists($this, $initializer)) {
-                $this->$initializer();
+        if (isset(static::$traitInitializers)) {
+            foreach (static::$traitInitializers[static::class] as $method) {
+                $this->{$method}();
+            }
+        } else {
+            foreach (Classes::classUsesDeep($this) as $traitNamespace) {
+                $ref = new \ReflectionClass($traitNamespace);
+                $traitName = $ref->getShortName();
+                $initializer = 'initialize' . $traitName;
+                if (method_exists($this, $initializer)) {
+                    $this->$initializer();
+                }
             }
         }
+
     }
 }
